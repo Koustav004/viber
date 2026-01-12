1dcff8c67e1cad51c29ea4b6cd6cfa3aad6625f6
Koustav004 <koustavrudra2004@gmail.com>
2026-01-12 22:18:04 +0530

    inngest setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,32 @@
-// import prisma from "@/lib/db";
+"use client";
 
-// const Page = async () => {
+import { toast } from "sonner";
+import { useMutation } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
 
-//   const users = await prisma.post.findMany();
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useState } from "react";
 
-//   return (
-//     <div>
-//       {JSON.stringify(users, null, 2)} 
-//     </div>
-//   );
-// }
-//  export default Page; 
+const Page = () => {
+const [value, setValue] = useState("");
+
+    const trpc = useTRPC();
+    const invoke = useMutation(trpc.invoke.mutationOptions({
+        onSuccess: () => {
+            toast.success("Background job started successfully");
+        }
+    }));
+
+    return (
+        <div className="p-4 max-w-7xl mx-auto">
+            <Input value={value} onChange={(e) => setValue(e.target.value)} />
+            <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value : value })}>
+                invoke Background Job
+            </Button>
+        </div>
+    );
+};
+
+export default Page;
+
