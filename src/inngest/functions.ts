1f02e7eb18386570d913bd242f620bb2e9ad5f06
Koustav004 <koustavrudra2004@gmail.com>
2026-01-18 16:25:23 +0530

    sandbox Setup done

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,25 +1,40 @@
 import { gemini, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
 
 
 import { inngest } from "./client";
+import { get } from "http";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("viberv0-dev");
+      return sandbox.sandboxId;
+    });
     const codeWriterAgent = createAgent({
-  name: 'summarizer',
+  name: 'code-writer',
   system:
-    'You are a expert summarizer. you take long text and summarize it in a 1 sentence.',
+    'you write code snippets based on user requests. only respond with code snippets, no explanations.',
   model: gemini({
     model: "gemini-3-flash-preview",
     apiKey: process.env.GEMINI_API_KEY,
   }),
 });
 const { output } = await codeWriterAgent.run(
-  `summarize the following text in a concise manner: ${event.data.value}`,
+  `code: ${event.data.value}`,
 );
 
-    return {output};
+const sandboxUrl = await step.run("get-sandbox-url", async () => {
+  const sandbox = await getSandbox(sandboxId);
+  const host = sandbox.getHost(3000);
+  return `http://${host}`;
+});
+
+
+
+    return {output, sandboxUrl};
   },
 );
