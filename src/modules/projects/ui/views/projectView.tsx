ae3722851e4500a91a56b4fb2522f9e91ca0d0a8
Koustav004 <koustavrudra2004@gmail.com>
2026-03-03 01:13:05 +0530

    Fragment View completed

--- src/modules/projects/ui/views/projectView.tsx
+++ src/modules/projects/ui/views/projectView.tsx
@@ -4,34 +4,42 @@ import { Suspense } from "react";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { MessagesContainer } from "../components/messagesContainer";
 import { useTRPC } from "@/trpc/client"
+import { useState } from "react";
+import { ProjectHeader } from "../components/projectHeader";
 
 import {
     ResizableHandle,
     ResizablePanel,
     ResizablePanelGroup
 } from "@/components/ui/resizable"
+import { Fragment } from "@/generated/prisma";
+import { FragmentWeb } from "../components/fragmentWeb";
 
 interface Props{
     projectId: string;
 };
 
 export const ProjectView = ({projectId}: Props) => {
-    // const trpc = useTRPC();
-    // const { data: project } = useSuspenseQuery(trpc.messages.getOne.queryOptions({
-    //     id: projectId,
-    // }))
+    const[activeFragment, setActiveFragment] = useState<Fragment | null>(null);
 
 
     return (
         <div className="h-screen">
                 <ResizablePanelGroup  className="h-full w-full flex flex-col">
                     <ResizablePanel
-                        defaultSize={35}
-                        minSize={26}
+                        defaultSize={20}
+                        minSize={15}
                         className="flex flex-col min-h-0"
                     >
+                        <Suspense fallback={<p>Loading Project...</p>}>
+                        <ProjectHeader projectId={projectId} />
+                        </Suspense>
                         <Suspense fallback={<p>Loading Messages...</p>}>
-                            <MessagesContainer projectId={projectId}/>
+                            <MessagesContainer 
+                            projectId={projectId}
+                            activeFragment={activeFragment}
+                            setActiveFragment={setActiveFragment}
+                            />
                         </Suspense>
                     </ResizablePanel>
                     <ResizableHandle withHandle/>
@@ -39,7 +47,7 @@ export const ProjectView = ({projectId}: Props) => {
                         defaultSize={65}
                         minSize={50}
                     >
-                        TODO: Preview
+                        {!!activeFragment && <FragmentWeb data = {activeFragment} />}
                     </ResizablePanel>
                 </ResizablePanelGroup>
         </div>
